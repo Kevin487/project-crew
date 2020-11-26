ba992cb8629485ecd7a80ba3daa3b66293b27b38
solisa986 <solisa@allegheny.edu>
2020-11-25 21:13:28 -0600

    inserted the try catch block

--- src/main/java/project/Main.java
+++ src/main/java/project/Main.java
@@ -19,10 +19,14 @@ public class Main {
     System.out.println("Cipher 1: Caesarian Cipher");
     System.out.println("Cipher 2:");
     System.out.println("Cipher 3:");
-    Scanner scanChoice = new Scanner(System.in);
-    System.out.println("");
-    System.out.println("Enter \"1\", \"2\" or \"3\"");
-    int choiceEntry = scanChoice.nextInt();
+    try {
+      Scanner scanChoice = new Scanner(System.in);
+      System.out.println("");
+      System.out.println("Enter \"1\", \"2\" or \"3\"");
+      int choiceEntry = scanChoice.nextInt();
+    } catch (Exception e) {
+      System.out.println("User inputted an incorrect choice. Please try again.");
+    }
   }
 
 }
