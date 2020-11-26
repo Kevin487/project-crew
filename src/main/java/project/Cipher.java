e44b8cfd97ee4777422824549d35fae0867e4f1c
solisa986 <solisa@allegheny.edu>
2020-11-25 20:49:10 -0600

    tweaking the file

--- src/main/java/project/Cipher.java
+++ src/main/java/project/Cipher.java
@@ -1,30 +1,32 @@
-package practicalfour.repeat;
+package project;
 
 /**
- * An interface for all of the repeater implementations.
+ * An interface for all of the cipher implementations.
  *
- * @author Janyl Jumadinova
+ * @author Adriana Solis
+ * @author Gabe Schwartz
+ * @author Kevin Lee
  */
 
-public abstract class Repeater {
+public abstract class Cipher {
 
-  /** The name of the repeater. */
+  /** The name of the cipher. */
   protected String name;
 
-  /** The default name of the repeater. */
-  private static final String DEFAULT_REPEATER = "default";
+  /** The default name of the cipher. */
+  private static final String DEFAULT_CIPHER = "default";
 
-  /** Configure the name of the repeater. */
-  public Repeater() {
-    name = DEFAULT_REPEATER;
+  /** Configure the name of the cipher. */
+  public Cipher() {
+    name = DEFAULT_CIPHER;
   }
 
-  /** The name of the repeater. */
+  /** The name of the cipher. */
   public String getName() {
     return name;
   }
 
-  /** Require that a Repeater provide a method for performing repeated string building. */
-  public abstract String repeat(char character, int repeats);
+  /** Require that a Cipher provide a method for performing encryptions/decryptions. */
+  // public abstract String encrypt(char character, int repeats);
 
 }
