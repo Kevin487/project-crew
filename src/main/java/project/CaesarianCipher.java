567a2ef3da99ec8876c85c5886d6c8b4545b7cba
solisa986 <solisa@allegheny.edu>
2020-11-26 00:05:30 -0600

    finished the caesar cipher part of the program

--- src/main/java/project/CaesarianCipher.java
+++ src/main/java/project/CaesarianCipher.java
@@ -10,9 +10,47 @@ import java.util.Scanner;
  * @author Gabe Schwartz
  */
 
-//public class CaesarianCipher {
+public class CaesarianCipher {
+  /* value of the alphabet. */
+  public static final String alphabet = "abcdefghijklmnopqrstuvwxyz";
 
- //public static void main(String args[]) {
-   //Scanner sc = new Scanner(System.in);
-   //System.out.println(" Input the plaintext message : ");
- //}
+  /* start of the encryption method for the user generated input. */
+  public static String encryptedMessage(String input, int shifter) {
+    // converts the input from the user to lowercase for simplicity
+    input = input.toLowerCase();
+    // initializing an empty string
+    String output = "";
+    // assigns an integer value to each letter of the alphabet
+    for (int i = 0; i < input.length(); i++) {
+      int value = alphabet.indexOf(input.charAt(i));
+
+      // finds out what the value of the shift is
+      int shiftValue = (shifter + value) % 26;
+      // finds out what character needs to be replaced
+      char replacedValue = alphabet.charAt(shiftValue);
+      // adding this to the output
+      output = output + replacedValue;
+    }
+    return output.toString();
+  }
+  /* start of the decryption method from the user generated input */
+  public static String decryptedMessage(String output, int shifter) {
+    // converst the output into lowercase for simplicity
+    output = output.toLowerCase();
+    // initializes an empty String
+    String input = "";
+
+    for (int i = 0; i < output.length(); i++) {
+      int value = alphabet.indexOf(output.charAt(i));
+      int shiftValue = (value - shifter) % 26;
+      // adding this if statement in case there are any negatives
+      if (shiftValue < 0) {
+        shiftValue = alphabet.length() + value;
+      }
+      // shows the character that is being replaced
+      char replacedValue = alphabet.charAt(shiftValue);
+      input = input + replacedValue;
+    }
+    return input.toString();
+  }
+}
--- src/main/java/project/Main.java
+++ src/main/java/project/Main.java
@@ -1,31 +1,72 @@
 package project;
 
+import java.lang.*;
 import java.util.Scanner;
+import java.util.*;
+
 
 
 /**
- * Class containing the main method.
- *
- * @author Adriana Solis
- * @author Kevin Lee
- * @author Gabe Schwartz
- */
+* Class containing the main method.
+*
+* @author Adriana Solis
+* @author Kevin Lee
+* @author Gabe Schwartz
+*/
 
 public class Main {
 
-  // TODO: add your implementation of the main method
-  public static void main(String args[]) {
-    System.out.println("Choose your cipher");
+  // add your implementation of the main method
+  public static void main(String[] args) {
+    final int VALUE_LOWER_LIMIT = 1;
+    final int VALUE_MIDDLE_LIMIT = 2;
+    final int VALUE_UPPER_LIMIT = 3;
+
+    System.out.println("Welcome to \"Useful Tool for Spies\", a Cipher program!");
+    System.out.println();
+    System.out.println("Please choose your desired cipher from the availabel options below.");
+    System.out.println();
     System.out.println("Cipher 1: Caesarian Cipher");
-    System.out.println("Cipher 2:");
-    System.out.println("Cipher 3:");
-    try {
-      Scanner scanChoice = new Scanner(System.in);
-      System.out.println("");
-      System.out.println("Enter \"1\", \"2\" or \"3\"");
-      int choiceEntry = scanChoice.nextInt();
-    } catch (Exception e) {
-      System.out.println("User inputted an incorrect choice. Please try again.");
+    System.out.println("Cipher 2: Vigenere Cipher");
+    System.out.println("Cipher 3: Java Cipher");
+    System.out.println();
+    Scanner scanChoice = new Scanner(System.in);
+    System.out.println("Enter \"1\", \"2\" or \"3\": ");
+    System.out.println();
+    int choiceEntry = scanChoice.nextInt();
+    if (choiceEntry == VALUE_LOWER_LIMIT) {
+      System.out.println();
+      System.out.println("You have chosen the Caesarian Cipher!");
+      System.out.println();
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Please enter the message that you want to encrypt: ");
+      String userInput = scanner.next();
+      System.out.println();
+      System.out.println("Please enter a shift value from 0-25: ");
+      System.out.println();
+      int shiftedValue = scanner.nextInt();
+      CaesarianCipher encrypt = new CaesarianCipher();
+      String encryptedMessages = encrypt.encryptedMessage(userInput, shiftedValue);
+      System.out.println("Encrypted Text:");
+      System.out.println(encryptedMessages);
+      System.out.println();
+      CaesarianCipher decrypt = new CaesarianCipher();
+      String decryptedMessages = decrypt.decryptedMessage(encryptedMessages, shiftedValue);
+      System.out.println("Decrypted Text:");
+      System.out.println(decryptedMessages);
+      scanner.close();
+      System.out.println();
+      System.out.print("Thank you for using \"Useful Tool for Spies\", a Cipher Program!");
+      System.out.print(" Have a nice day!");
+      System.out.println();
+    } else if (choiceEntry == VALUE_MIDDLE_LIMIT) {
+      System.out.println();
+      System.out.println("You have chosen the Vigenere Cipher!");
+    } else if (choiceEntry == VALUE_UPPER_LIMIT) {
+      System.out.println();
+      System.out.println("You have chosen the Java Cipher!");
+    } else {
+      System.out.println("Please try again with a valid choice.");
     }
   }
 
