a4dcd34a0cfbdc4ddc02cf1612473f857ae903d5
Kevin487 <54716696+Kevin487@users.noreply.github.com>
2020-12-10 04:28:44 -0500

    test java cipher

--- src/test/java/project/TestCaesarianCipher.java
+++ src/test/java/project/TestCaesarianCipher.java
@@ -5,16 +5,17 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- * A JUnit test suite.
- *
- * @author Adriana Solis
- * @author Kevin Lee
- * @author Gabe Schwartz
- */
+* A JUnit test suite.
+*
+* @author Adriana Solis
+* @author Kevin Lee
+* @author Gabe Schwartz
+*/
 
 public class TestCaesarianCipher {
   // add your implementation of the test cases
   private CaesarianCipher cipher = new CaesarianCipher();
+  private VigenereCipher cipher2 = new VigenereCipher();
 
   /* Tests that the cipher should do nothing with an empty message. */
   @Test
@@ -56,5 +57,18 @@ public class TestCaesarianCipher {
     String message = "hello";
     String encrypt = cipher.encryptedMessage(message, 3);
     assertEquals(message, cipher.encryptedMessage(encrypt, -3));
+
+
+    String key1 = "key";
+    String key2 = "qwe";
+    String key3 = "zxc";
+    String key4 = "iop";
+
+    @Test
+    public void cipherShouldEncryptLowercaseLetters() {
+      assertEquals("kfa", cipher2.encryptInput("abc", key1));
+      assertEquals("qxg", cipher2.encryptInput("abc", key2));
+      assertEquals("zye", cipher2.encryptInput("abc", key3));
+      assertEquals("ipr", cipher2.encryptInput("abc", key4));
+    }
   }
-}
