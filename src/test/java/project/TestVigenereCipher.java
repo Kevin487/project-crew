64b30dd88d13589002b148a3cb357b7bf41118dc
solisa98 <solisa@alegheny.edu>
2020-12-10 04:34:41 -0500

    doubling experiment?

--- src/test/java/project/TestVigenereCipher.java
+++ src/test/java/project/TestVigenereCipher.java
@@ -20,6 +20,10 @@ import static org.junit.Assert.*;
    String key2 = "qwe";
    String key3 = "zxc";
    String key4 = "iop";
+   String key5 = "KEY";
+   String key6 = "QWE";
+   String key7 = "ZXC";
+   String key8 = "IOP";
 
    @Test
    public void cipherShouldEncryptLowercaseLetters() {
@@ -27,5 +31,13 @@ import static org.junit.Assert.*;
      assertEquals("qxg", cipher.encryptInput("abc", key2));
      assertEquals("zye", cipher.encryptInput("abc", key3));
      assertEquals("ipr", cipher.encryptInput("abc", key4));
+   }
+   
+   @Test
+   public void cipherShouldEncryptUppercaseLetters() {
+     assertEquals("KFA", cipher.encryptInput("ABC", key5));
+     assertEquals("QXG", cipher.encryptInput("ABC", key6));
+     assertEquals("ZYE", cipher.encryptInput("ABC", key7));
+     assertEquals("IPR", cipher.encryptInput("ABC", key8));
    }
 }
